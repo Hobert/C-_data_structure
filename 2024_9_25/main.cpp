14dc1a520f357820184e7afe051901641f6cab47
Hobert <2716412428@qq.com>
2024-09-25 22:44:48 +0800

    1

---
+++ 2024_9_25/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+template<class T1,class T2> 
+class Person
+{
+	public:
+		Person(T1 name,T2 age);
+		void showPerson();
+		T1 m_Name;
+		T2 m_Age;
+};
+
+template<class T1,class T2> 
+Person<T1,T2>::Person(T1 name,T2 age)
+{
+	
+	this->m_Name = name;
+	this->m_Age = age;
+	
+}
+
+
+template<class T1,class T2> 
+void Person<T1,T2>::showPerson()
+{
+	cout << "Name:" << this->m_Name << endl;
+	cout << "Age:" << this->m_Age << endl;
+	 
+}
+
+
+void test01()
+{
+	Person<string,int> P1("Akebi",14);
+	P1.showPerson();
+}
+
+int main(int argc, char** argv) {
+	
+	test01();
+	return 0;
+}
